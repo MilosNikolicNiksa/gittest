1f6a62444d77667e1febe575efc90dbbfa878907
Milos Nikolic <nmilos93.mn@gmail.com>
2017-10-30 12:00:24 +0100

    First git commit

---
+++ GITCommit/src/MainClass.java
@@ -0,0 +1,16 @@
+import java.io.Console;
+
+public class MainClass
+{
+
+    public static void main(String[] args)
+    {
+	String[] names= {"Milos","Nikola","Stefan","Marko","Jelena"};
+	
+	for(int i=0; i<names.length; i++){
+	    System.out.println(names[i]);
+	}
+
+    }
+
+}
