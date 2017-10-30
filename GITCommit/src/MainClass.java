d011cca3b3f938aa3e90363971a997c2c29029e4
Milos Nikolic <nmilos93.mn@gmail.com>
2017-10-30 12:01:54 +0100

    Second commit

--- GITCommit/src/MainClass.java
+++ GITCommit/src/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass
 
     public static void main(String[] args)
     {
-	String[] names= {"Milos","Nikola","Stefan","Marko","Jelena"};
+	String[] names= {"Milos","Nikola","Stefan","Marko","Jelena","Ana"};
 	
 	for(int i=0; i<names.length; i++){
 	    System.out.println(names[i]);
